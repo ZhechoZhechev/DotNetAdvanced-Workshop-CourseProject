0179d4a3728a95d170d25bf914b5b59099d51eb7
ZhechoZhechev <mishomishev024@gmail.com>
2024-02-07 17:10:19 +0200

    Added custom error pages.

--- HouseRentingSystem.Web/Program.cs
+++ HouseRentingSystem.Web/Program.cs
@@ -51,7 +51,8 @@ public class Program
         }
         else
         {
-            app.UseExceptionHandler("/Home/Error");
+            app.UseExceptionHandler("/Home/Error/500");
+            app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
             app.UseHsts();
         }
 
