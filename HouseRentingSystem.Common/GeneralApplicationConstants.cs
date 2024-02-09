f3959309d07abd2b9a7ca2ff87a1efe21d8d0df1
ZhechoZhechev <mishomishev024@gmail.com>
2024-02-09 17:17:16 +0200

    Base admin controller added.

--- HouseRentingSystem.Common/GeneralApplicationConstants.cs
+++ HouseRentingSystem.Common/GeneralApplicationConstants.cs
@@ -6,4 +6,8 @@ public static class GeneralApplicationConstants
 
     public const int CurrentPageConst = 1;
     public const int HousesPerPageConst = 3;
+
+    public const string AdminAreaName = "Admin";
+    public const string AdminRoleName = "Administrator";
+    public const string DevelopmentAdminEmail = "admin@houserenting.bg";
 }
